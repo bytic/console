7eb9d3af958ddb3066fbc84c66a10e2968025a35
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-10-28 15:38:40 +0200

    add application class

---
+++ src/Application.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ByTIC\Console;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application extends SymfonyApplication
+{
+
+}
--- src/Command.php
+++ src/Command.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace ByTIC\Console;
+
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 class Command extends SymfonyCommand
