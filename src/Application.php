86652c815c59ecf03f73a9ce9ea8919b0b121158
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-11-19 15:29:51 +0200

    update composer references

--- src/Application.php
+++ src/Application.php
@@ -4,7 +4,10 @@ namespace ByTIC\Console;
 
 use Symfony\Component\Console\Application as SymfonyApplication;
 
+/**
+ * Class Application
+ * @package ByTIC\Console
+ */
 class Application extends SymfonyApplication
 {
-
 }
--- src/Command.php
+++ src/Command.php
@@ -4,7 +4,10 @@ namespace ByTIC\Console;
 
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
+/**
+ * Class Command
+ * @package ByTIC\Console
+ */
 class Command extends SymfonyCommand
 {
-
 }
